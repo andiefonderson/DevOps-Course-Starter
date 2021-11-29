21ab3462c817d37393736cdb36bcca07b900612e
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-29 12:12:54 +0000

    Added in functionalities to display the to-do list and to add new tasks to the to-do list. Added a form in the HTML file to let a user submit new tasks for their to-do list. Started adding in functionality to edit the to-do list.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+# from session_items import *
 
+from todo_app.data.session_items import *
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -7,5 +9,11 @@ app.config.from_object(Config())
 
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index():    
+    list = get_items()
+    return render_template("index.html", to_do_items=list)
+
+@app.route('/', methods=['POST'])
+def add_to_do():
+    add_item(request.form.get('to-do-title'))
+    return redirect('/')
