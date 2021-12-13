b5bf6eb33c868c3c8b82bbc38a0f2ee5c73d461e
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-12-13 15:35:54 +0000

    Linked the API call to the To-Do App. Installed requests to allow the API caller to work. At the moment only getting all tasks on the index page and viewing individual tasks is linked to the Trello board.

--- todo_app/data/api_caller.py
+++ todo_app/data/api_caller.py
@@ -1,4 +1,4 @@
-from flask import json, request
+from flask import json
 import os
 import requests
 
@@ -12,13 +12,21 @@ def get_tasks():
         "token":api_token, 
         "fields":"name", 
         "cards":"all", 
-        "card_fields":"name"}
+        "card_fields":"name,desc"}
     response = requests.get(api_url, params=auth_string)
     list_cards = json.loads(response.text)
+    
+    todo_tasks = []
+
     for list in list_cards:
-        print("Status: " + list['name'])
         for card in list['cards']:
-            print("Task: " + card['name'])
+            task = { 'id': card['id'], 'title': card['name'], 'status': list['name'], 'notes': card['desc']}
+            todo_tasks.append(task)
+    
+    return todo_tasks
 
-
-get_tasks()
+def get_task(id):
+    tasks = get_tasks()
+    for task in tasks:
+        if id == task['id']:
+            return task
